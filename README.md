e170c6741bd9ee13e99a3999081beb74c1cbfc4a
cd223 <chrisdavies870@gmail.com>
2018-10-17 16:22:58 +0100

    Refactoring db access

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# MindPalaceService
+# Mind Palace Service
 A RESTful service for CRUD operations on Mind Palace project data.
+
+## Usage
+REST API to be added...
+
+### List all devices
+
+**Definition**
+
+`GET /notes`
+
+**Response**
+
+- `200 OK` on success
--- myapp.py
+++ myapp.py
@@ -5,34 +5,38 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/notes')
+def get_db_connection():
+    url = urlparse.urlparse(os.environ['DATABASE_URL'])
+    dbname = url.path[1:]
+    user = url.username
+    password = url.password
+    host = url.hostname
+    port = url.port
+    connection = psycopg2.connect(dbname=dbname, user=user, password=password, host=host, port=port)
+    return connection
+
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    cursor.execute("""SELECT * from note""")
+    return cursor.fetchall()
+
+@app.route('/notes', methods=['GET', 'POST'])
 def get_notes():
-    conn = ""
-    out = []
+    connection = ""
+    response = []
     try:
-        url = urlparse.urlparse(os.environ['DATABASE_URL'])
-        dbname = url.path[1:]
-        user = url.username
-        password = url.password
-        host = url.hostname
-        port = url.port
-        conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host, port=port)
+        connection = get_db_connection()
     except:
-        out = {"err": "Unable to connect to the database"}
-        return jsonify(out), 404
+        response = {"err": "Unable to connect to the database"}
+        return jsonify(response), 404
         
     try:
-        cur = conn.cursor()
-        cur.execute("""SELECT * from note""")
-        rows = cur.fetchall()
-        out = []
-        for row in rows:
-            out.append({"id": row[0], "name": row[1]})
+        response = execute_query(connection, """SELECT * from note""")
     except:
-        out = {"err": "General SQL Error"}
-        return jsonify(out), 404
+        response = {"err": "General SQL Error"}
+        return jsonify(response), 404
 
-    return jsonify(out), 200
+    return jsonify(response), 200
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
