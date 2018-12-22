86821fb4a72297870fbca46a4be3fea7d5aab064
cd223 <chrisdavies870@gmail.com>
2018-12-22 13:00:00 +0000

    Testing progress endpoint

--- myapp.py
+++ myapp.py
@@ -182,7 +182,7 @@ def progress():
     total = []
     for elem in all_notes:
         total.append(int(elem['count']))
-    remembered_notes = get_data("""SELECT COUNT(note_id) from note WHERE note_status=true AND palace_id=(SELECT palace_id from palace WHERE palace_title='%s');""", (AsIs(title),), True)
+    remembered_notes = get_data("""SELECT COUNT(note_id) from note WHERE note_status=true AND palace_id=(SELECT palace_id from palace WHERE palace_title='%s');""", (AsIs(title),), False)
     remembered = []
     for elem in remembered_notes:
         remembered.append(int(elem['count']))
